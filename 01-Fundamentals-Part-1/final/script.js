6835942a2f9619f0f0e9d3da2b7a5843c222bf30
Johnson <johnsonkarasanyi@gmail.com>
2022-06-01 12:44:20 +0200

    Review on data types

--- 01-Fundamentals-Part-1/final/script.js
+++ 01-Fundamentals-Part-1/final/script.js
@@ -2,18 +2,18 @@
 ////////////////////////////////////
 // Linking a JavaScript File
 let js = "amazing";
-console.log(40 + 8 + 23 - 10);
+//console.log(40 + 8 + 23 - 10  );
 
 ////////////////////////////////////
 // Values and Variables
-console.log("Jonas");
-console.log(23);
+//console.log("Jonas" );
+//console.log(23);
 
 let firstName = "Matilda";
 
-console.log(firstName);
-console.log(firstName);
-console.log(firstName);
+//console.log(firstName);
+//console.log(firstName);
+//console.log(firstName);
 
 // Variable name conventions
 // let jonas_matilda = "JM";
@@ -32,25 +32,25 @@ console.log(firstName);
 
 ////////////////////////////////////
 // Data Types
-// let javascriptIsFun = true;
-// console.log(javascriptIsFun);
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
 
-// console.log(typeof true);
-//console.log(typeof javascriptIsFun);
-// console.log(typeof 23);
-// console.log(typeof 'Jonas');
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'Jonas');
 
-// javascriptIsFun = 'YES!';
-// console.log(typeof javascriptIsFun);
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
 
-// let year;
-// console.log(year);
-// console.log(typeof year);
+let year;
+console.log(year);
+console.log(typeof year);
 
-// year = 1991;
-// console.log(typeof year);
+year = 1991;
+console.log(typeof year);
 
-// console.log(typeof null);
+console.log(typeof null);
 
 ////////////////////////////////////
 // let, const and var
