ad8e70779c90a8ebe708bcfcb185b629db4b80af
Johnson <johnsonkarasanyi@gmail.com>
2022-06-01 13:41:33 +0200

    Review on Js Operators

--- 01-Fundamentals-Part-1/final/script.js
+++ 01-Fundamentals-Part-1/final/script.js
@@ -1,7 +1,7 @@
 
 ////////////////////////////////////
 // Linking a JavaScript File
-let js = "amazing";
+//let js = "amazing";
 //console.log(40 + 8 + 23 - 10  );
 
 ////////////////////////////////////
@@ -9,7 +9,7 @@ let js = "amazing";
 //console.log("Jonas" );
 //console.log(23);
 
-let firstName = "Matilda";
+//let firstName = "Matilda";
 
 //console.log(firstName);
 //console.log(firstName);
@@ -32,25 +32,25 @@ let firstName = "Matilda";
 
 ////////////////////////////////////
 // Data Types
-let javascriptIsFun = true;
-console.log(javascriptIsFun);
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
 
-console.log(typeof true);
-console.log(typeof javascriptIsFun);
-console.log(typeof 23);
-console.log(typeof 'Jonas');
+// console.log(typeof true);
+// console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Jonas');
 
-javascriptIsFun = 'YES!';
-console.log(typeof javascriptIsFun);
+// javascriptIsFun = 'YES!';
+// console.log(typeof javascriptIsFun);
 
-let year;
-console.log(year);
-console.log(typeof year);
+// let year;
+// console.log(year);
+// console.log(typeof year);
 
-year = 1991;
-console.log(typeof year);
+// year = 1991;
+// console.log(typeof year);
 
-console.log(typeof null);
+// console.log(typeof null);
 
 ////////////////////////////////////
 // let, const and var
@@ -59,10 +59,10 @@ console.log(typeof null);
 
 // const birthYear = 1991;
 // birthYear = 1990;
-// const job;
 
 // var job = 'programmer';
 // job = 'teacher'
+// console.log(job)
 
 // lastName = 'Schmedtmann';
 // console.log(lastName);
@@ -75,8 +75,8 @@ console.log(typeof null);
 // const ageSarah = now - 2018;
 // console.log(ageJonas, ageSarah);
 
-//console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
-// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+// console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// //2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
 
 // const firstName = 'Jonas';
 // const lastName = 'Schmedtmann';
